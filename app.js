501927d4aaa2486a15f447a8d436dc0cc0d582a0
SERKAN AKIN <serkan.akin@netcad.com>
2017-03-13 13:53:49 +0300

    reprojection ok

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const sequelize = require("sequelize");
 const morgan	= require("morgan");
 const socketIO	= require("socket.io");
 const http		= require("http");
-var port=process.env.PORT||3006;
+var port=process.env.PORT||3005;
 
 app.use(morgan('dev'));
 app.use(bodyparser.urlencoded({extended: true}));
@@ -17,7 +17,7 @@ app.use(express.static('static'));
 var server 	= http.createServer(app);
 var io 		= socketIO(server);
 
-var db = new sequelize("postgres://postgres:pi@www.akinba.com:5432/puhu");
+var db = new sequelize("postgres://postgres:pi@www.akinba.com:5432/edremit");
 var bina= db.define('bina',
 {
 	gid: {
@@ -29,7 +29,7 @@ var bina= db.define('bina',
 		type: sequelize.STRING
 	},
 	geom: {
-		type: sequelize.GEOMETRY('POLYGON',4326)
+		type: sequelize.GEOMETRY('Polygon',4326)
 	}
 },
 {
@@ -40,17 +40,35 @@ db.sync({force: false});
 bina.findOrCreate(
 {
 	where: {
-		gid: 2
+		gid: 3
 	},
 	defaults: {
 		bina_adi: 'test',
 		geom: {
-			type: 'POLYGON',
-			coordinates: [[100.0, 0.0], [101.0, 0.0], [101.0, 1.0],[100.0, 1.0], [100.0, 0.0]],
+			type: 'Polygon',
+			coordinates: [
+				[[26.9668150773239,39.5980355463212],
+				[26.9669486288099,39.5980339361376],
+				[26.9669409282849,39.597970679018],
+				[26.9668196798476,39.5979788857023],
+				[26.9668150773239,39.5980355463212]]
+			],
 			crs: {type: 'name', properties: {name: 'EPSG:4326'}}
 		}
 	}
 }).spread((bina,created)=>{});
+
+/*bina.create({bina_adi: 'test', geom: { type: 'Polygon', coordinates: [
+			[[26.9668150773239,39.5980355463212],
+			[26.9669486288099,39.5980339361376],
+			[26.9669409282849,39.597970679018],
+			[26.9668196798476,39.5979788857023],
+			[26.9668150773239,39.5980355463212]]
+                ],
+            	crs: { type: 'name', properties: { name: 'EPSG:4326'}}}
+            }).then((ress)=>{
+            	console.log(ress);
+            });*/
 
 app.get('/',(req,res)=>{
 	db.query("select json_build_object(\
@@ -59,12 +77,11 @@ app.get('/',(req,res)=>{
 				json_build_object(\
 				'type', 'Feature',\
 				'properties', json_build_object(\
-				'gid', gid,\
-				'bina_adi', bina_adi\
+				'gid', gid \
 				) :: JSON,\
 				'geometry', st_asgeojson(geom) :: JSON)\
 				)\
-				) from bina",
+				) from bina where gid<500",
 	{type: sequelize.QueryTypes.SELECT}
 	).then((data)=>{
 		console.log(data[0].json_build_object);
